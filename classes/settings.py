88bcd33ff3d3deaf87dd62a1c1d179d085b45d61
Eugene <viiper94@gmail.com>
2023-11-16 18:39:49 +0200

    fixed getting correct app path

--- classes/settings.py
+++ classes/settings.py
@@ -1,19 +1,15 @@
-import sys
 import os
 
 
 class Settings:
 
-    # Change path prefix for .exe
-    # path_prefix = sys._MEIPASS        # path for Windows
-    path_prefix = os.path.dirname(os.path.abspath(__file__))        # path for Mac OS
-    # path_prefix = '.'                 # path for development
-
-    poppler_path = 'lib/poppler-osx/bin'
+    path_prefix = os.path.dirname(os.path.abspath(__file__))
+    poppler_path = 'lib/poppler/bin'
     tmp_path = 'tmp'
     template_path = 'template/default.pptx'
 
     resolution = 1080
+
     dpi = 300
 
     def __init__(self):
@@ -37,16 +33,15 @@ class Settings:
 
     @staticmethod
     def get_poppler_path():
-        prefix = Settings.path_prefix.replace('/classes', '')
+        prefix = Settings.path_prefix.replace('classes', '')
         return os.path.join(prefix, Settings.poppler_path)
 
     @staticmethod
     def get_template_path():
-        prefix = Settings.path_prefix.replace('/classes', '')
+        prefix = Settings.path_prefix.replace('classes', '')
         return os.path.join(prefix, Settings.template_path)
 
     @staticmethod
     def get_tmp_folder_path():
-        prefix = Settings.path_prefix.replace('/classes', '')
+        prefix = Settings.path_prefix.replace('classes', '')
         return os.path.join(prefix, Settings.tmp_path)
-
