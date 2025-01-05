4fd30ef42d1a212c73bf3390db9b4e1db6c68dc9
Eugene <viiper94@gmail.com>
2025-01-05 20:24:50 +0200

    added password parameter, raising PDFIncorrectPasswordError if poppler returns PDFPageCountError

--- classes/info_handler.py
+++ classes/info_handler.py
@@ -1,9 +1,18 @@
 from pdf2image import pdfinfo_from_path
+from pdf2image.exceptions import PDFPageCountError
 from classes.settings import Settings
+from classes.exceptions.incorrect_password_error import PDFIncorrectPasswordError
 
 
 class InfoHandler:
 
     @staticmethod
-    def get_pdf_metadata(file):
-        return pdfinfo_from_path(file, poppler_path=Settings.get_poppler_path())
+    def get_pdf_metadata(file, password=None):
+        data = {}
+        try:
+            data = pdfinfo_from_path(file, poppler_path=Settings.get_poppler_path(), userpw=password)
+        except PDFPageCountError as e:
+            if 'Incorrect password' in str(e):
+                raise PDFIncorrectPasswordError
+
+        return data
